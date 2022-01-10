42da7a3e3b2e01891b5b706769637f575f66deab
Yashashvi <yashashvi.p@devtrust.biz>
2022-01-10 21:00:08 +0530

    First commit

---
+++ build/static/js/10.be74dcb9.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonptech-mania"]=this["webpackJsonptech-mania"]||[]).push([[10],{84:function(e,t,i){"use strict";i.r(t);i(0);var a=i(4);t.default=function(){return Object(a.jsx)("video",{autoPlay:!0,loop:!0,muted:!0,id:"video",children:Object(a.jsx)("source",{src:"./video/galaxy.mp4",type:"video/mp4"})})}}}]);
+//# sourceMappingURL=10.be74dcb9.chunk.js.map
